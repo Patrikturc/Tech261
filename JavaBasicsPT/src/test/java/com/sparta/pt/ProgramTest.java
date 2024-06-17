928a8ac98f85c4bd3f914f55e3cfc518424e023a
Patrik Turcani <pturcani@spartaglobal.com>
2024-06-17 15:10:09 +0100

    Added JUnit, testing class and a simple test

---
+++ JavaBasicsPT/src/test/java/com/sparta/pt/ProgramTest.java
@@ -0,0 +1,11 @@
+package com.sparta.pt;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void basicTest(){
+        Assertions.assertTrue(5 == 5);
+    }
+}
