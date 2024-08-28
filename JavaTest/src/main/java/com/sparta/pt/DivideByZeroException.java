224553b602d01df58720005c00c860a8f5545c46
Patrik Turcani <patrikturc@gmail.com>
2024-08-28 09:41:12 +0100

    Refactoring custom exception

--- JavaTest/src/main/java/com/sparta/pt/DivideByZeroException.java
+++ JavaTest/src/main/java/com/sparta/pt/DivideByZeroException.java
@@ -2,10 +2,6 @@ package com.sparta.pt;
 
 public class DivideByZeroException extends RuntimeException {
 
-    public DivideByZeroException() {
-        super("Cannot Divide By Zero");
-    }
-
     public DivideByZeroException(String message) {
         super(message);
     }
